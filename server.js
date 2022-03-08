b89f85721a7f210bbd3799a2a93a01ccf15a4194
Senna Annaba Ahmad <71185988+SeeN-Zero@users.noreply.github.com>
2022-03-08 15:51:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -21,9 +21,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: "159.20.14.100",
+    language: "en-US,en;q=0.9,id;q=0.8",
+    software: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
+  })
+})
 
 
 
